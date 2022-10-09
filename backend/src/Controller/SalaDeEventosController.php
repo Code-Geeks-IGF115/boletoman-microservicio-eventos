a6c918a768ecfec4c85fc0534bfff4e425064eca
Cargueta01 <aa20099@ues.edu.sv>
2022-10-09 23:41:10 +0000

    agregado nuevo

--- backend/src/Controller/SalaDeEventosController.php
+++ backend/src/Controller/SalaDeEventosController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 #[Route('/sala/de/eventos')]
 class SalaDeEventosController extends AbstractController
 {
@@ -39,6 +40,21 @@ class SalaDeEventosController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    #[Route('/sala', name: 'app_sala_de_eventos_crear', methods: ['POST'])]
+    public function sala(Request $request, SalaDeEventosRepository $salaDeEventosRepository): Response
+    {
+        $salaDeEvento = new SalaDeEventos();
+        $form = $this->createForm(SalaDeEventosType::class, $salaDeEvento);
+        $form->handleRequest($request);
+        $lista = array($_GET);
+
+        return $this->json_encode{
+            $mensaje: "",
+            estado: 200 | 403 (no tiene permisos) | 400 (datos incorrectos)
+        }
+    }
+
 
     #[Route('/{id}', name: 'app_sala_de_eventos_show', methods: ['GET'])]
     public function show(SalaDeEventos $salaDeEvento): Response
