9a294325cfcc2e5eae547fe64e8068c0ad6dae75
Cargueta01 <aa20099@ues.edu.sv>
2022-10-12 03:38:36 +0000

    funcion editar sala ya no se necesita modificar nada

--- backend/src/Controller/SalaDeEventosController.php
+++ backend/src/Controller/SalaDeEventosController.php
@@ -53,26 +53,10 @@ class SalaDeEventosController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-
-            try{
-
                 $salaDeEventosRepository->save($salaDeEvento, true);//para guardar en base de datos
                 $result= $serializer->serialize(['message'=>"Sala de Eventos Guardada."],'json');
-                return $response->fromJsonString($result);
-
-            }catch(Exception $e){
-                $result= $serializer->serialize(['message'=>"Datos no válidos."],'json');
-                $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
-                return $response->fromJsonString($result);
-            }
-
-        }      
-
-
-            $salaDeEventosRepository->save($salaDeEvento, true);//para guardar en base de datos
-            $result= $serializer->serialize(['message'=>"Sala de Eventos Guardada."],'json');
-            //return $response->fromJsonString($result);
-        }    
+                return $response->fromJsonString($result);         
+        }       
         else{
             $result= $serializer->serialize(['message'=>"Datos no válidos."],'json');
             $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
@@ -131,7 +115,7 @@ class SalaDeEventosController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $salaDeEventosRepository->save($salaDeEvento, true);
-            $result= $serializer->serialize(['message'=>"Sala de Eventos sobreescrita con exito."],'json');
+            $result= $serializer->serialize(['message'=>"Sala de Eventos se modificó con exito."],'json');
         }
         else{
             $result= $serializer->serialize(['message'=>"Datos no válidos."],'json');
